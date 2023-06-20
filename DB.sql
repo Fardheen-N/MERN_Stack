959c38128f09eba6a1253db6e83718c3e18cf386
Fardheen-N <fardheen.n2021csea@sece.ac.in>
2023-06-20 09:24:49 +0530

    Added avergae and count cmd

--- DB.sql
+++ DB.sql
@@ -13,11 +13,26 @@ insert into student values(7,'samantha','F','sam@gmail.com');
 insert into student values(8,'dharsan','M','PG@gmail.com');
 insert into student values(9,'dhanush','M','DDM@gmail.com');
 insert into student values(10,'Ana de armas','F','anadeArmas@gmail.com');
-select * from student order by Name;
 select Name from student where Gender = 'M';
-
+-- HW
 create table tutor(Id int,Name varchar,Gender varchar);
 insert into tutor values(1,'Raylieh','M');
 insert into tutor values(2,'raiden','F');
 select * from tutor ;
-select student.Name,tutor.Name from student inner join tutor on student.Gender = tutor.Gender;
+
+alter table student add mark int;
+update student set mark = 89 where Id=1;
+update student set mark = 98 where Id=2;
+update student set mark = 91 where Id=3;
+update student set mark = 100 where Id=4;
+update student set mark = 67 where Id=5;
+update student set mark = 78 where Id=6;
+update student set mark = 52 where Id=7;
+update student set mark = 90 where Id=8;
+update student set mark = 47 where Id=9;
+update student set mark = 82 where Id=10;
+
+select * from student;
+select student.Name,tutor.Name from student inner join tutor on student.Gender = tutor.Gender; 
+select avg(mark) from student;
+select count(Name) from student;
