25e68ded3e4cccf01fec252ecc0f3cae3a89472b
Fardheen-N <fardheen.n2021csea@sece.ac.in>
2023-06-24 15:22:35 +0530

    day 6 (3.22)

--- node/day6/block.js
+++ node/day6/block.js
@@ -1,19 +1,78 @@
 const fs = require('fs')
-const {add,multi}= require('./app');
-// console.log(fs)
-fs.readFile(`${__dirname}/states.txt`,(err,data)=>{
-    if(err) console.log("Error");
-    else console.log(data.toString());
-})
-const inputData = 'bankai kathon karamath shinju ⚔️'
-fs.writeFile(`${__dirname}/bleach.txt`,inputData,(err)=>{
-    if(err) console.log("error while writing")
-    else console.log("done writing");
-})
+// const {add,multi}= require('./app');
+// // console.log(fs)
+// // fs.readFile(`${__dirname}/states.txt`,(err,data)=>{
+// //     if(err) console.log("Error");
+// //     else console.log(data.toString());
+// // })
+// // const inputData = 'bankai kathon karamath shinju ⚔️'
+// // fs.writeFile(`${__dirname}/bleach.txt`,inputData,(err)=>{
+// //     if(err) console.log("error while writing")
+// //     else console.log("done writing");
+// // })
 
-fs.readFile(`${__dirname}/bleach.txt`,(err,data)=>{
-    if(err) console.log("Error");
-    else console.log(data.toString());
-})
-const name = 'chandraaaaaaaaaa'
-console.log(`Hello ${name}`);
+// // fs.readFile(`${__dirname}/bleach.txt`,(err,data)=>{
+// //     if(err) console.log("Error");
+// //     else console.log(data.toString());
+// // })
+// const name = 'chandraaaaaaaaaa'
+// console.log(`Hello ${name}`);
+
+function read(filepath){
+    return new Promise((resolve,reject) => {
+        fs.readFile(filepath,(err,data) => {
+            if(err) reject("Failed during read")
+            else resolve(data)
+        })
+    })
+} 
+
+function write(input,filepath){
+    return new Promise((resolve,reject) => {
+        fs.writeFile(input,filepath,(err) => {
+            if(err) reject("Failed during write")
+            else resolve('written')
+        })
+    })
+} 
+
+const input = 'yokoso vathakshiva soul socity hado-no 99'
+read(`bleach.txt`)
+.then(()=> write('bleach.txt',input))
+.then((data)=>console.log(data.toString()))
+.catch((err)=>console.log("Some error occured"))
+
+// // read(`${__dirname}/bleach.txt`)
+// // console.log(write(input,`${__dirname}/bleach.txt`))
+
+// const fs = require('fs')
+
+// function read(filepath){
+//     return new Promise((resolve,reject) => {
+//         fs.readFile(filepath,(err,data) => {
+//             if(err) reject("Failed during read")
+//             else resolve(data)
+//         })
+//     })
+// } 
+
+// function write(filepath,update){
+//     return new Promise((resolve,reject) => {
+//         fs.writeFile('bleach.txt', update, (err) => {
+//             if (err) reject("error");
+//             else resolve('The file has been saved!');
+//           });
+//     })
+// } 
+
+
+// read('bleach.txt')
+// .then((data)=>{
+//     console.log(data.toString())
+//     write('bleach.txt',"Ani!!")
+// .then((data)=>{
+//     console.log(data.toString())
+// })
+// .catch((err)=>console.log("Some error occured"))
+// })
+// .catch((err)=>console.log(err))
