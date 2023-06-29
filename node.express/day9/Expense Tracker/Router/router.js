bc9739f5b9b78349846a1b19b0a9c2e6cc9d7717
Fardheen-N <fardheen.n2021csea@sece.ac.in>
2023-06-29 09:49:24 +0530

    api completed

--- node.express/day9/Expense Tracker/Router/router.js
+++ node.express/day9/Expense Tracker/Router/router.js
@@ -3,9 +3,9 @@ const Expense = require('../models/expense')
 const router = express.Router();
 
 router.post("/expense/create",createExpense)
-router.get('/expense/get')
-router.patch('/expense/edit',editExpense)
-router.delete('expense/delete/:id')
+router.get('/expense/get/:id',getAll)
+router.patch('/expense/edit/:id',editExpense)
+router.delete('/expense/delete/:id',deleteExpense)
 
 async function createExpense(req,res){
     let body = req.body;
@@ -22,6 +22,34 @@ async function createExpense(req,res){
     res.status(201).json('expense created')
 }
 async function editExpense(req,res){
-    
+    try {
+        const {id}=req.params;
+        const expense = await Expense.findByIdAndUpdate(id,req.body);
+        if(!expense) res.status(404).json('id not found');
+        else res.status(201).json('updated');
+    } catch (error) {
+        return res.status(404).send('entity not edited');
+    }
 }
+async function getAll(req,res){
+    try {
+        const {id} = req.params;
+        const expense = await Expense.findById(id);
+        if(!expense) return res.status(404).send(`cannot find the expense entity with id :${id}`)
+        res.status(201).json(expense);
+    } catch (error) {
+        return res.status(404).send('entity not found');
+    }
+}
+async function deleteExpense(req,res){
+    try {
+        const {id} = req.params;
+        const expense = await Expense.findByIdAndDelete(id);
+        if(!expense) return res.status(404).json({message:`cannot find the expense entity with id :${id}`})
+        else res.status(201).send('Entity deleted');
+    } catch (error) {
+        return res.status(404).send('entity not found');
+    }
+}
+
 module.exports = router;
